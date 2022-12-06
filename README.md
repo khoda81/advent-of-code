0205b1c93fd8c1dabe55863eec2f73ef575af456
axiom <20.mahdikh.0@gmail.com>
2022-12-06 19:12:15 +0330

    added readme.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# My Personal AOC setup
+
+## USAGE
+
+1. Clone the repo.
+2. Replace `TOKEN` in `./.env` file with advent of code session id ([how to get](https://github.com/wimglenn/advent-of-code-wim/issues/1)).
+
+This token is used to automatically get personal inputs from advent of code server. You can disable this in by passing --no-input to create-workspace. DO NOT COMMIT THIS TOKEN TO A PUBLIC REPOSITORY.
+
+## Files
+
+- `boilerplate.py`: this is the solution boilerplate that will be copied as solution.py file into the solution folder.
+
+- `create_workspace.py`:
+    1. create puzzle folder (`./{year}/{day}` by default)
+    2. copy boilerplate file into the folder
+    3. wait until puzzle is opened to download the input file
+
+- `organize.py`: helper for organizing messy solution files into `{year}/{day}` format.
+
+Run boilerplate.py with --tomorrow a few minutes before the puzzle. Then open the website, wait until its unlocked. When you're done reading the puzzle, solution boilerplate and input file are ready in the folder for you.
--- create_workspace.py
+++ create_workspace.py
@@ -1,29 +1,65 @@
-from logging import getLogger, basicConfig, INFO
-from pathlib import Path
-import shutil
 import argparse
-from aocd import models, get, exceptions, utils
-from dotenv import load_dotenv
+import shutil
+from logging import INFO, basicConfig, getLogger
+from pathlib import Path
 
-
-basicConfig(level=INFO)
 BOILERPLATE_PATH = Path("boilerplate.py")
+SOLUTION_NAME = "solution.py"
 log = getLogger(__name__)
-load_dotenv()
 
 
-def main(args):
-    session = None
-    year = args.year
-    day = args.day
-    block = not args.no_block
-    quiet = args.quiet
-    tomorrow = args.tomorrow
+def main(args: argparse.Namespace) -> None:
+    from aocd import exceptions, models, utils
+    from dotenv import load_dotenv
 
-    if session is None:
-        user = models.default_user()
+    load_dotenv()
+
+    if not args.quiet:
+        basicConfig(level=INFO)
+
+    year, day = get_date(args.year, args.day, args.tomorrow)
+
+    # create puzzle folder
+    path = Path(f"{year}/{day}/")
+    path.mkdir(parents=True, exist_ok=True)
+
+    # copy boilerplate and rename to solution.py
+    solution_path = path / SOLUTION_NAME
+
+    if solution_path.is_file():
+        log.warning(f"{solution_path} already exists, skipping")
     else:
-        user = models.User(token=session)
+        log.info(f"copying {BOILERPLATE_PATH} to {solution_path}")
+        shutil.copyfile(BOILERPLATE_PATH, solution_path)
+
+    if not args.no_input:
+        if args.session is None:
+            user = models.default_user()
+        else:
+            user = models.User(token=args.session)
+
+        puzzle = models.Puzzle(year=year, day=day, user=user)
+
+        try:
+            input_data = puzzle.input_data
+        except exceptions.PuzzleLockedError:
+            if args.no_block:
+                raise
+
+            utils.blocker(quiet=args.quiet, until=(year, day))
+            input_data = puzzle.input_data
+
+        log.info(f"loading puzzle input for {year=}, {day=}")
+
+        # write input data to input.txt
+        input_path = path / "input.txt"
+        input_path.write_text(input_data)
+    
+    log.info("done")
+
+
+def get_date(year=None, day=None, tomorrow=False):
+    from aocd import get
 
     if day is None:
         day = get.current_day()
@@ -39,49 +75,63 @@ def main(args):
         year = get.most_recent_year()
         log.info("most recent year=%s", year)
 
-    puzzle = models.Puzzle(year=year, day=day, user=user)
-
-    path = Path(f"{year}/{day}/")
-    path.mkdir(parents=True, exist_ok=True)
-
-    # copy boilerplate and rename to solution.py
-    solution_path = path / "solution.py"
-    log.info(f"copying {BOILERPLATE_PATH} to {solution_path}")
-    if solution_path.is_file():
-        log.warning(f"{solution_path} already exists, skipping")
-    else:
-        shutil.copyfile(BOILERPLATE_PATH, solution_path)
-
-    try:
-        input_data = puzzle.input_data
-    except exceptions.PuzzleLockedError:
-        if not block:
-            raise
-
-        utils.blocker(quiet=quiet, until=(year, day))
-        input_data = puzzle.input_data
-
-    log.info(f"loading puzzle input for {year=}, {day=}")
-
-    # write input data to input.txt
-    input_path = path / "input.txt"
-    input_path.write_text(input_data)
+    return year, day
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        prog='create_workspace',
+        prog="create_workspace",
         description=(
             "Create workspace for Advent of Code puzzle. "
-            "Running with no puzzle date will block until today's puzzle is available. "
+            "Running with --tomorrow will block until puzzle is available. "
             "This Behavior can be disabled with the --no-block flag."
-        )
+        ),
     )
 
-    parser.add_argument("-y", "--year", type=int, default=None)
-    parser.add_argument("-d", "--day", type=int, default=None)
-    parser.add_argument("-n", "--no-block", action="store_true")
-    parser.add_argument("-q", "--quiet", action="store_true")
-    parser.add_argument("-t", "--tomorrow", action="store_true")
+    parser.add_argument(
+        "-y",
+        "--year",
+        type=int,
+        default=None,
+        help="year of the puzzle, defaults to most recent year",
+    )
+    parser.add_argument(
+        "-d",
+        "--day",
+        type=int,
+        default=None,
+        help="day of the puzzle, defaults to current day",
+    )
+    parser.add_argument(
+        "-n",
+        "--no-block",
+        action="store_true",
+        help="do not block until puzzle is unlocked",
+    )
+    parser.add_argument(
+        "-i",
+        "--no-input",
+        action="store_true",
+        help="do not download puzzle input",
+    )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="do not print status messages",
+    )
+    parser.add_argument(
+        "-t",
+        "--tomorrow",
+        action="store_true",
+        help="get tomorrow's puzzle instead of today's",
+    )
+    parser.add_argument(
+        "-s",
+        "--session",
+        type=str,
+        default=None,
+        help="session cookie for aocd",
+    )
     args = parser.parse_args()
     main(args)
