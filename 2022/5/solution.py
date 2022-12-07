0646ca4993ef8fdeea2d4cb441a10b656c1299f2
axiom <20.mahdikh.0@gmail.com>
2022-12-07 09:53:21 +0330

    2022-7

--- 2022/5/solution.py
+++ 2022/5/solution.py
@@ -7,7 +7,7 @@ import re
 import sys
 
 
-INPUT_FILE = Path(__file__).parent / "input.txt"
+INPUT_FILE = Path(__file__).parent / "input(1).txt"
 
 
 def main() -> None:
@@ -23,9 +23,33 @@ def solution(inp):
     result = 0
     items = []
 
-    for line in inp:
-        print(line)
+    next_line = next(inp)
+    n_stacks = (len(next_line) + 1) // 4
+    stacks = [[] for _ in range(n_stacks)]
 
+    while next_line != '\n':
+        line, next_line = next_line, next(inp)
+
+        for i, stack in enumerate(stacks):
+            item = line[4*i + 1]
+
+            if item != ' ':
+                stacks[i].append(item)
+
+    for stack in stacks:
+        stack.reverse()
+
+    for line in inp:
+        _, count, _, from_,_, to_ = line.split(" ")
+        count = int(count)
+        from_ = int(from_) - 1
+        to_ = int(to_) - 1
+
+        items = stacks[from_][-count:]
+        stacks[from_] = stacks[from_][:-count]
+        stacks[to_].extend(items)
+
+    print(''.join([stack.pop() for stack in stacks]))
     print(result)
 
 
--- 2022/7/solution.py
+++ 2022/7/solution.py
@@ -7,26 +7,115 @@ import re
 import sys
 
 
-INPUT_FILE = Path(__file__).parent / "input.txt"
+INPUT_PATH = Path(__file__).parent / "input.txt"
 
 
 def main() -> None:
-    std_mode = 0
+    input_path = INPUT_PATH
+    if not input_path.is_file():
+        input_path = 0 # open(0) will open stdin as a file
 
-    if not INPUT_FILE.is_file() or std_mode:
-        solution(sys.stdin)
-    else:
-        with open(INPUT_FILE, "r") as f:
-           solution(f)
+    with open(input_path, "r") as f:
+        solution(f)
 
 def solution(inp):
-    result = 0
-    items = []
+    root = {}
+    root['.']= '/'
+    root[".."] = root
+    
+    current = root
 
-    for line in inp:
-        print(line)
+    def get_path(folder):
+        if folder == root:
+            return "/"
 
-    print(result)
+        path = []
+        while folder != root:
+            path.append(folder['.'])
+            folder = folder['..']
+
+        return "/".join(reversed(path))
+
+    running = True
+    command = next(inp)
+    while running:
+        if command.startswith("$ ls"):
+            for line in inp:
+                if line.startswith("$"):
+                    command = line
+                    break
+
+                a, b = line.strip().split()
+                if a.isnumeric():
+                    current[b] = int(a)
+                else:
+                    if b not in current:
+                        current[b] = new_folder = {}
+                        new_folder['.'] = b
+                        new_folder['..'] = current
+            else:
+                running = False
+        else:
+            path = command.split()[2]
+            if path == '/':
+                current = root
+            else:
+                current = current[path]
+
+            command = next(inp)
+
+    def size(folder):
+        result_size = 0
+        for k, v in folder.items():
+            if k == '.' or k == '..':
+                continue
+
+            if isinstance(v, int):
+                result_size += v
+            else:
+                result_size += size(v)
+
+        return result_size
+    
+    def print_tree(folder, indent=0):
+        print("  " * indent, "-", folder["."], "(dir)")
+        
+        for k, v in folder.items():
+            if k in ['.', ".."]:
+                continue
+            
+            if isinstance(v, int):
+                print("  " * indent, "  -", k, v)
+            else:        
+                print_tree(v, indent + 1)
+
+    total = 70_000_000
+    empty = total - size(root)
+    required = 30_000_000 - empty
+    print(required)
+
+    result = float("inf")
+    def find_best(folder):
+        nonlocal result
+        result_size = 0
+        for k, v in folder.items():
+            if k == '.' or k == '..':
+                continue
+
+            if isinstance(v, int):
+                result_size += v
+            else:
+                result_size += find_best(v)
+
+        if result_size > required:
+            result = min(result, result_size)
+
+        return result_size
+    
+    # print_tree(root)
+    find_best(root)
+    print("result:", result)
+    # print(sum(i for i in size(root) if i <= 100_000))
 
 
 if __name__ == "__main__":
